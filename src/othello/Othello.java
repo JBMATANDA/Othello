c115d9fc2c3acc45ed9eee6c487b6e1910e6decf
carlitos79 <carlitos792001@yahoo.es>
2016-10-24 08:07:32 +0200

    Othello with a thread dedicated to hold the board

--- src/othello/Othello.java
+++ src/othello/Othello.java
@@ -4,7 +4,9 @@ Othello är spelets huvudklass och (eftersom spelet är en JaxaFX applikation)
  */
 package othello;
 
+import othello.interfaces.GameBoard;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 import participants.*;
 
@@ -13,13 +15,47 @@ import participants.*;
  * @author Group 4
  * 
  */
-public class Othello/* extends Application*/{
+public class Othello extends Application{
+    
+    GameBoard board = new GameBoard();
+    private boolean wakeful = true;
+    
+    /**
+     * Here the thread that holds the board is created
+     */
+    @Override public void start(Stage primaryStage) {
+        
+     new Thread(new Runnable() {
+         
+      @Override public void run() {
+          
+        try {
+            
+          while (wakeful) {
+            
+            Platform.runLater(new Runnable() {
+                
+              @Override public void run() {
+                board.initBoard(primaryStage);
+                
+                 //Debbuging line...                 
+                System.out.println("Board's thread ID number: " + Thread.currentThread().getId());
+              }
+            });
+            
+            Thread.sleep(1000);
+          }
+        }
+        catch (InterruptedException ex) {  }
+      }
+    }).start();
+  }    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
-        GameManager gm = new GameManager();
-        gm.run();
+        
+        Application.launch(args);
     }
 }
