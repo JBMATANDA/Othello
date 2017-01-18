5f8dc9fcd4535a2ad074f3ecedb34b5ff35d874b
carlitos79 <carlitos792001@yahoo.es>
2017-01-18 10:24:01 +0100

    Othello with a thread dedicated to hold the board... funkar

--- src/participants/RemoteComputerPlayer.java
+++ src/participants/RemoteComputerPlayer.java
@@ -13,10 +13,72 @@ Servern upptäcka att klient-Socketen har stängts och avsluta tråden på ett k
  */
 package participants;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import othello.DatabaseManager;
+
 /**
  *
  * @author optimusprime
  */
-public class RemoteComputerPlayer/* extends Player*/{
+public class RemoteComputerPlayer extends Application {
     
+    private TextArea textArea = new TextArea();
+    private int clientNo = 0;
+    private int groupNo = 5;
+    
+    @Override public void start(Stage primaryStage){
+     
+       Scene scene = new Scene(new ScrollPane(textArea), 450, 195);
+       primaryStage.setTitle("Othello Server");
+       primaryStage.setScene(scene);
+       primaryStage.show();
+       
+       new Thread( () -> {
+           
+           try {
+               
+               ServerSocket serverSocket = new ServerSocket(8000);
+               textArea.appendText("Othello multithreaded Server started...\n");            
+               
+               while(true){
+                   
+                   Socket clientSocket = serverSocket.accept();
+                   clientNo++;
+                   
+                   Platform.runLater( () -> {
+                       
+                       InetAddress clientConnectInfo = clientSocket.getInetAddress();
+                   
+                       textArea.appendText("Starting a thread for client: " + clientNo + '\n');
+                       textArea.appendText("The client's IP address is: " + clientConnectInfo.getHostAddress() + '\n');                       
+                       textArea.appendText("The client is using port nummer: 8000\n");
+                       DatabaseManager.addClient(clientNo, clientConnectInfo.getHostAddress(), 8000);
+                   });
+                   
+                   new Thread(new ClientService(clientSocket, textArea)).start();
+               }
+               
+           } catch (IOException ex) {
+               
+               Logger.getLogger(RemoteComputerPlayer.class.getName()).log(Level.SEVERE, null, ex);
+           }
+           
+       }).start();        
+    }
+    
+    public static void main(String[] args){
+        
+        launch(args);
+    }
 }
